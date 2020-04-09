0de3d20fe4a83f5897282f4979fec7ee2843d4c0
sunidhi chaudhary <sunidhi.nith@gmail.com>
2020-04-09 22:05:21 +0530

    initialized project

---
+++ src/main/java/com/sapient/week1/CheckEven.java
@@ -0,0 +1,23 @@
+package com.sapient.week1;
+
+public class CheckEven {
+	public static boolean check(int n) {
+		boolean result = false;
+        if(n%2 == 0){
+            result = true;
+        }
+        return result;
+	}
+		 
+	 
+	public static void main(String[] args) {
+//		 check(5);
+		 check(4);
+		 check(2);
+//		 check(123);
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ src/test/java/com/sapient/week1/CheckEvenTest.java
@@ -0,0 +1,19 @@
+package com.sapient.week1;
+import org.junit.Test;
+
+public class CheckEvenTest {
+	
+	 public void testEven() {
+		CheckEven even = new CheckEven(); 
+		assertEquals("4 is a even number",true,even.check(4));
+			
+		
+        
+     
+    }
+
+	private void assertEquals(String string, boolean b, Boolean check) {
+		// TODO Auto-generated method stub
+		
+	}
+}
